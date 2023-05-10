f12d21904e7b15cba63954610fc23d7e5822d252
KelvinMomanyi <kelvinmata42@gmail.com>
2023-05-10 09:21:55 +0300

    start

---
+++ react.js
@@ -0,0 +1,10 @@
+const App=()=>{
+    return React.createElement('div',{},[
+        React.createElement('h1',{class:'title'}, 'react is rendered')
+    ])
+}
+
+ReactDOM.render(
+ React.createElement(App),
+ document.getElementById('root')
+)
